faab0bed82e0c36abc6c5ae8390b7039e9611825
ramatejarayar <122608308+ramatejarayar@users.noreply.github.com>
2023-05-07 12:26:17 +0530

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,22 @@
+
+    <><link rel="stylesheet" type="text/css" href="style.css"></link><srcipt></srcipt></></>
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
+function addUser()
+{
+    user_name = document.getElementById("user_name").value;
+    firebase.database().ref("/").child(user_name).update({
+        purpose : "adding user"
+    });
+    /*
+    localStorage.setItem("user_name", user_name);
+
+    window.location = "chat_room.html";
+    */
+}
+
+
+
